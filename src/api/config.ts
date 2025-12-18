84e4fa9fdac719824ed582692a0af1441686042a
Sergey Belov <kort.dll@gmail.com>
2025-12-18 17:40:04 +0300

    up fo prod

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,1 +1,5 @@
-export const API_BASE_URL = "http://localhost:3000/api";
+const isProduction = import.meta.env.MODE === "production";
+
+export const API_BASE_URL = isProduction
+  ? "https://loremo.dpdns.org/api"
+  : "http://localhost:3000/api";
